993c76f852e365c5e148bfa961d2fe7c0cd1a459
sunjunjian <samsun_wsu@yahoo.com>
2012-09-01 21:41:07 -0700

    Add iterators and Shell Sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -20,9 +20,111 @@ struct gen_rand
 		}
 	};
 
+
+
+// -------------------------------------
+// insertion sort regular version
+// -------------------------------------
+template	<typename	elemType>
+void	sort_1( vector<elemType>	&vec )
+{
+	int 	i;
+	elemType	key;
+	int 	num_items = vec.size();
+
+	for ( int j = 1; j < num_items; ++j )
+	{
+		key = vec[j];
+		i = j-1;
+
+		while ( (i >= 0) && (vec[i] < key ) )
+		{
+			vec[i+1] = vec[i];
+			--i;
+		};
+
+		vec[i+1] = key;
+	}
+}
+
+
+
+// -------------------------------------
+// insertion sort using iterators
+// -------------------------------------
+// !!! MUST use IterType   !!!
+// -------------------------------------
+template	<typename	 IterType, typename		elemType>
+void	sort_2( IterType first, IterType last, elemType foo )
+{
+	IterType 	it_current = first;
+	IterType 	iter;
+	elemType	key;
+
+
+	for ( ++it_current; it_current != last; ++it_current )
+	{
+		key = *it_current;
+		iter = it_current-1;
+
+		while ( (iter >= first) && ( (*iter) > key ) )
+		{
+			*(iter+1) = *iter;
+			--iter;
+		};
+
+		*(iter+1) = key;
+	}
+}
+
+
+
+
+
+
+// -------------------------------------
+//       shell  sort
+// -------------------------------------
+//template	<typename	 IterType, typename		elemType>
+void	shell_sort( vector<double> &vec )
+{
+	int 			gap;
+	int 			i, j, k;
+	double 			temp;
+	int 			num_items = vec.size();
+
+	int 			a[] = { 701, 301, 132, 57, 23, 10, 4, 1 };
+	vector<int>		gaps( a, a+8 );
+
+
+	for ( i = 0; i < gaps.size(); ++i )
+	{
+		gap = gaps[i];
+
+		// do an insertion sort for each gap size
+		for ( j = gap; j < num_items; ++j )
+		{
+			temp = vec[j];
+
+			for ( k = j; (k >= gap) && (vec[k-gap] > temp); k -= gap )
+			{
+				vec[k] = vec[k - gap];
+			};
+
+			vec[k] = temp;
+		}
+
+	}
+}
+
+
+
+
+
+
+
 int	main(int argc, char* argv[])
 {
-	vector<int>		iVec;
 	// ----------------------------------
 	// atoi may cause overflow, like 
 	// char			str[50];
@@ -39,27 +141,16 @@ int	main(int argc, char* argv[])
 	// ----------------------------------------
 	
 
-	std::vector<double>		dVec;
+	vector<double>		dVec;
 	dVec.reserve(num_items);
 	generate_n( std::back_inserter(dVec), num_items, gen_rand() );
 
-	// ---------------
-	// insertion sort
-	// ---------------
-	int 	i;
-	double	dKey;
 
-	for ( int j = 1; j < num_items; ++j )
-	{
-		dKey = dVec[j];
-		i = j-1;
-		while ( (i >= 0) && (dVec[i] < dKey ) )
-		{
-			dVec[i+1] = dVec[i];
-			--i;
-		};
-		dVec[i+1] = dKey;
-	}
+	// call sort function
+	//sort_1( dVec );
+	//sort_2( dVec.begin(), dVec.end(), *dVec.begin() );
+	shell_sort( dVec );
+
 
 	// print the vector
 	std::ostream_iterator<double>	os( cout, "\t\t" );
